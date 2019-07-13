8429344e3cd448cef8bacd547280ab18b350da4e
Xavier Orejarena <xavierorejarena@Xaviers-MacBook-Pro.local>
2019-07-13 19:36:38 -0400

    se agrega el archivo que contiene los comandos de git

--- display.php
+++ display.php
@@ -28,7 +28,7 @@ body {
   <img src="https://i0.wp.com/boxmining.com/wp-content/uploads/2017/08/binancelogoblack.jpg" width="200">
 </div>
 
-<div class="container col-lg-8"> 
+<div class="container col-lg-6"> 
   <table class="table table-bordered table-inverse table-hover">
     <thead>
       <tr>
