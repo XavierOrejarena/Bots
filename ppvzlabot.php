11f85d6f150cba41c68cad3cfb32cab36d45f13e
Xavier Orejarena <xavierorejarena@Xaviers-MacBook-Pro.local>
2019-07-14 12:34:35 -0400

    ppvzlabot ahora recibe mensajes directos.

--- ppvzlabot.php
+++ ppvzlabot.php
@@ -241,7 +241,7 @@ Total: ".number_format($USD, 2, ',', '')." Bs.";
 
             sendMessage($chat_id, $msg1);
             // if ($msg2 !== NULL) {
-                sendMessage($chat_id, "test");
+                sendMessage($chat_id, "test2");
             // }
             
         }
