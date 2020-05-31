14bbec77ab9a3df20e7a96fbeef9b2c3527c72aa
Xavier Orejarena <xavierorejarena@Xaviers-MacBook-Pro.local>
2020-05-31 19:43:39 -0400

    Test

--- the_binance_bot.php
+++ the_binance_bot.php
@@ -212,7 +212,7 @@ If you want to know all token listed in Binance.com just type /coins");
         sendMessage($chat_id, '"/BTCUSDT" command for USDT/BTC Price
 "/ALARM BTCUSDT 8500" to receive an alarm when btc reaches that rate.
 "/MYALARMS to manage all your alarms.
-CAPs does not matter.');
+upper case and lower case does not matter.');
     }
     elseif (strtolower($text) == "/coins") {
         $Binance = json_decode(file_get_contents("https://api.binance.com//api/v1/exchangeInfo"), true);
