450afafdd35a57d4d4be8394e06280e804b619c0
Xavier Orejarena <xavierorejarena@Xaviers-MacBook-Pro.local>
2020-06-01 01:16:08 -0400

    Test

--- gitPull.php
+++ gitPull.php
@@ -1,5 +1,4 @@
 #!/usr/bin/env php
 <?php
-echo exec('git reset --hard');
-echo exec('git pull https://github.com/XavierOrejarena/Bots.git');
+echo exec("git pull https://github.com/XavierOrejarena/Bots.git");
 ?>
