0efb0d7a34f99ead7d943c87344e61656d957392
Xavier Orejarena <xavierorejarena@Xaviers-MacBook-Pro.local>
2020-06-22 19:01:36 -0400

    Test

--- ppvzlabot.php
+++ ppvzlabot.php
@@ -221,10 +221,15 @@ function processMessage($message) {
 
             if ($signal == 'x' || $signal == '*' || is_null($signal) || $signal == 'X') {
                 $receive = round(($USD-$USD*(0.054)-0.3), 2);
-                $msg1 = "Envían: $USD $
-Llegarán: $receive $
-\xE2\x98\x95: $Bolivares Bs.
-Total: ".number_format($receive*$Bolivares, 2, ',', '')." Bs.";
+                if ($BS) {
+                    $msg1 = "Envían: $USD $
+    Llegarán: $receive $
+    \xE2\x98\x95: $Bolivares Bs.
+    Total: ".number_format($receive*$Bolivares, 2, ',', '')." Bs.";
+                } else {
+                    $msg1 = "Envían: $USD $
+    Llegarán: $receive $";
+                }
                 $sent = round((100*($USD+0.3)/94.6),2);
                 $msg2 = "Envían: $sent $
 Llegarán: $USD $
