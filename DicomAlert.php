6840905a8d81db07a83047181e4ee509ee274a46
Xavier Orejarena <xavierorejarena@Xaviers-MacBook-Pro.local>
2020-07-03 01:53:27 -0400

    Test

--- DicomAlert.php
+++ DicomAlert.php
@@ -23,15 +23,20 @@ $result = $link->query($sql);
 if ($result->num_rows > 0) {
     $OldText = mysqli_fetch_assoc($result)['tasa'];
 	if ($text != $OldText && strlen($text) > 7) {
+		if (is_numeric($text)) {
+			$sql = "UPDATE DICOM SET tasa = '$text' WHERE id = 1";
 
-		$sql = "UPDATE DICOM SET tasa = '$text' WHERE id = 1";
-
-		if ($link->query($sql) === TRUE) {
-			file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=$text");
-			// file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=old: $OldText");
+			if ($link->query($sql) === TRUE) {
+				file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=$text");
+				// file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=old: $OldText");
+			} else {
+				file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=149273661&text=error".$conn->error); 
+			}
 		} else {
-			file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=error".$conn->error); 
+			file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=149273661&text=$text"); 
 		}
+		
+
 	}
 } else {
     file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=0 Results");
