8617f37561a82e4a64365215be955907c4d40627
Xavier Orejarena <xavierorejarena@Xaviers-MacBook-Pro.local>
2020-07-16 20:59:52 -0400

    Ahora K mayuscula funciona asi como tambien k minuscula.

--- ApruebaBot.php
+++ ApruebaBot.php
@@ -226,8 +226,7 @@ function processMessage($message) {
             $USD = str_word_count($text, 1, '0123456789.')[0];
             $BS = str_word_count($text, 1, '0123456789.')[1];
             $signal = str_word_count($text, 1, '*xX/\def')[0];
-            $BS = pow(1000,strlen(strstr($BS, 'K')))*(real)$BS;
-            $BS = pow(1000,strlen(strstr($BS, 'k')))*(real)$BS;
+            $BS = pow(1000,strlen(strstr($BS, 'kK')))*(real)$BS;
 
             if ($signal == 'x' || $signal == '*' || is_null($signal) || $signal == 'X') {
                 $receive = round(($USD-$USD*(0.054)-0.3), 2);
