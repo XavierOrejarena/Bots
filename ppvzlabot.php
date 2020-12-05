80fd22f581ac010a8dc6fca605836f7dabadb5b2
Xavier Orejarena <xavierorejarena@Xaviers-MacBook-Pro.local>
2020-12-05 08:03:47 -0400

    Se quitaron decimales innecesarios cuando son ceros en el bot paypal

--- ppvzlabot.php
+++ ppvzlabot.php
@@ -146,7 +146,7 @@ function processQuery($inline_query)
         $USD = str_word_count($text, 1, '0123456789.')[0];
         $BS = str_word_count($text, 1, '0123456789.')[1];
         $signal = str_word_count($text, 1, '*xX/\def')[0];
-        $BS = pow(1000,strlen(stristr($BS, 'k')))*(real)$BS + 0;
+        $BS = floatval(pow(1000,strlen(stristr($BS, 'k')))*(real)$BS);
     }
 
     if (empty($inline_query['query'])) {
