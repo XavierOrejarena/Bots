a3254dd426079fd4bba8631e5c76bef6079481da
Xavier Orejarena <xavierorejarena@Xavier-MacBook-Pro.local>
2021-01-03 17:44:32 -0400

    Silkroad test1

--- Silkroad.php
+++ Silkroad.php
@@ -1,22 +1,6 @@
 <table>
 <?php 
-echo "PHP!<br>";
-if (!empty($_POST)) {
-    echo "<h2>PHP is Fun!</h2>";
-    echo "Hello world!<br>";
-    foreach ($_POST as $key => $value) {
-        echo "<tr>";
-        echo "<td>";
-        echo $key;
-        echo "</td>";
-        echo "<td>";
-        echo $value;
-        echo "</td>";
-        echo "</tr>";
-    }
-}else {
-    var_dump($_POST);
-}
+file_put_contents('test.txt', file_get_contents('php://input'));
 
 ?>
 </table>
