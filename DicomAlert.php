a9efe4964e837bfd2b564f642d62f3c85a2999b6
Xavier Orejarena <xavierorejarena@Xavier-MacBook-Pro.local>
2021-04-27 13:21:13 -0400

    Silkroad

--- DicomAlert.php
+++ DicomAlert.php
@@ -9,8 +9,8 @@ preg_match_all('/USD/', $data, $matches, PREG_OFFSET_CAPTURE);
 $text = substr($data, $matches[0][0][1]+85, 12);
 $text = (string)$text;
 $token = '16396100:AAG_6y_pnkgYCKNRMyFVHow2eefR719DfCk';
-// $chat_id = '@dicomalert';
-$chat_id = 149273661;
+$chat_id = '@dicomalert';
+// $chat_id = 149273661;
 
 $sql = "SELECT tasa FROM DICOM WHERE id = 1";
 $result = $link->query($sql);
