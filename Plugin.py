348e0fdf0289fce986adad12e70f9d81faa2f311
Xavier Orejarena <xavierorejarena@Xavier-MacBook-Pro.local>
2021-04-28 00:08:11 -0400

    Silkroad

--- Plugin.py
+++ Plugin.py
@@ -17,10 +17,11 @@ def handle_chat(t,player,msg):
 		i = 0
 		for x in get_inventory()['items']:
 			if x:
+				log(x['name'])
 				if x['name'] == 'Special Return Scroll':
 					Packet = bytearray()
-					Packet.append(i) # Inventory slot
-					Packet.append(0xEC) # Always constant = 0x0C30
+					Packet.append(i)
+					Packet.append(0xEC)
 					Packet.append(0x09)
 					inject_joymax(0x704C, Packet, True)
 					stop_bot()
@@ -28,6 +29,7 @@ def handle_chat(t,player,msg):
 					start_bot()
 					break
 			i+=1
+			
 	if t == 7 and bol and 'CONTROL BOT' in msg and name in players and Path(__file__).stem == '1auhASa1vckjbw2he-AS21FSADs':
 		data = re.findall(r'\d+', msg)
 		a = int(data[0])
