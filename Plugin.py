556ad0f741c373d640827e64a738d816b2245678
Xavier Orejarena <xavierorejarena@Xavier-MacBook-Pro.local>
2021-05-07 19:54:42 -0400

    Silkroad

--- Plugin.py
+++ Plugin.py
@@ -68,8 +68,8 @@ def teleport(source,destination):
 				return
 
 def tlp(x):
-	phBotChat.Private('Nelliel1123','tlp,'+x[1]+','+x[2])
-	phBotChat.Private('BLACKandBLUE','tlp,'+x[1]+','+x[2])
+	phBotChat.Private('Nelliel1123',x[1]+','+x[2])
+	phBotChat.Private('BLACKandBLUE',x[1]+','+x[2])
 	log('Teleported by Command')
 	return 0
 
