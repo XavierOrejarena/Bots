1b0e2498846b352f5063242fdb18431df72e0e08
Xavier Orejarena <xavierorejarena@Xavier-MacBook-Pro.local>
2021-05-07 17:53:20 -0400

    Silkroad

--- Plugin.py
+++ Plugin.py
@@ -10,7 +10,25 @@ from threading import Timer
 players = ['chapito01','chapito02']
 
 def handle_chat(t,player,msg):
-	log('XD')
+	log("XD")
+	global players
+	name = get_character_data()['name']
+
+	if t == 7 and 'CONTROL BOT' in msg and name in players and Path(__file__).stem == '1auhASa1vckjbw2he-AS21FSADs':
+		log("Maximiliano es gay")
+		data = re.findall(r'\d+', msg)
+		a = int(data[0])
+		b = int(data[1])
+		c = int(data[2])
+
+		if msg.find('mas') < 0:
+			result = a-b
+		else:
+			result = a+b
+
+		result = result*c
+		log(str(result))
+		phBotChat.All(str(result))
 
 log("*** Sell Merca ***")
 
