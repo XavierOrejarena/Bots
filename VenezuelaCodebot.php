1b7316a6bd9c708837964e16c3b156b9c9f3bbdb
Xavier Orejarena <xavierorejarena@Xavier-MacBook-Pro.local>
2021-07-16 22:02:53 -0400

    Silkroad

--- VenezuelaCodebot.php
+++ VenezuelaCodebot.php
@@ -136,7 +136,7 @@ function processMessage($message) {
       $COD = $text[1];
       // $sql = "SELECT $SYMBOL FROM venezuela WHERE id = $COD";
       // $result = $link->query($sql);
-      apiRequestWebhook("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => $SYMBOL));
+      apiRequestWebhook("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => $COD));
     }
   } else {
     apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'I understand only text messages'));
