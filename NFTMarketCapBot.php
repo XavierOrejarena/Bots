cbf3da9597218478b9b7c1236301f588c5ab74ff
Xavier Orejarena <xavierorejarena@Xavier-MacBook-Pro.local>
2021-09-22 14:25:48 -0400

    Silkroad

--- NFTMarketCapBot.php
+++ NFTMarketCapBot.php
@@ -125,38 +125,39 @@ function processMessage($message) {
   $message_id = $message['message_id'];
   $chat_id = $message['chat']['id'];
   $text = $message['text'];
-  $text = str_word_count($text, 1, "0123456789.");
-  $TOKEN = strtoupper($text[0]);
-
-  $url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest';
-  $parameters = [
-    'symbol' => $TOKEN
-  ];
-
-  $headers = [
-    'Accepts: application/json',
-    'X-CMC_PRO_API_KEY: 2d6611e3-3184-4874-9ed1-c7c67c7a9c94'
-  ];
-  $qs = http_build_query($parameters); // query string encode the parameters
-  $request = "{$url}?{$qs}"; // create the request URL
-
-
-  $curl = curl_init(); // Get cURL resource
-  // Set cURL options
-  curl_setopt_array($curl, array(
-    CURLOPT_URL => $request,            // set the request URL
-    CURLOPT_HTTPHEADER => $headers,     // set the headers 
-    CURLOPT_RETURNTRANSFER => 1         // ask for raw response instead of bool
-  ));
-
-  $response = curl_exec($curl); // Send the request, save the response
-  $data = json_decode($response);
-  $price = (round($data->data->$TOKEN->quote->USD->price,2)); // print json decoded response
-  curl_close($curl); // Close request
+  
 
   if (strpos($text, "/start") === 0) {
     apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => "Los tokens NFT más populares son /PVU /WANA /CCAR /AXS"));
   } else {
+    $text = str_word_count($text, 1, "0123456789.");
+    $TOKEN = strtoupper($text[0]);
+
+    $url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest';
+    $parameters = [
+      'symbol' => $TOKEN
+    ];
+
+    $headers = [
+      'Accepts: application/json',
+      'X-CMC_PRO_API_KEY: 2d6611e3-3184-4874-9ed1-c7c67c7a9c94'
+    ];
+    $qs = http_build_query($parameters); // query string encode the parameters
+    $request = "{$url}?{$qs}"; // create the request URL
+
+
+    $curl = curl_init(); // Get cURL resource
+    // Set cURL options
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => $request,            // set the request URL
+      CURLOPT_HTTPHEADER => $headers,     // set the headers 
+      CURLOPT_RETURNTRANSFER => 1         // ask for raw response instead of bool
+    ));
+
+    $response = curl_exec($curl); // Send the request, save the response
+    $data = json_decode($response);
+    $price = (round($data->data->$TOKEN->quote->USD->price,2)); // print json decoded response
+    curl_close($curl); // Close request
     apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => $price));
   }
 }
