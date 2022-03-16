5fd9859670d81c1df686ba8f791af8f0e41d775f
Xavier Orejarena <xavierorejarena@Macintoshs-MacBook-Pro.local>
2022-03-15 20:44:11 -0400

    Silkroad

--- Plugins.py
+++ Plugins.py
@@ -67,8 +67,7 @@ def handle_event(t, data):
 		Timer(1.0, os.kill, (os.getppid(), 9)).start()
 		Timer(2.0, os.kill, (os.getpid(), 9)).start()
 	if t == 2 and Path(__file__).stem == 'Plug' and thief and data not in Players:
-		Packet = bytearray()
-		inject_joymax(0x704C, Packet, True)
+		inject_joymax(0x704C, bytearray(), False)
 		log(data)
 		lru = '_LHuSAEVa7VbqI/sendMessage?chat_id=774088349&text='
 		url = 'https://api.telegram.org/bot1221990015:AAHlL2X_NInc3xNo9MEnX' + lru
@@ -79,8 +78,7 @@ def handle_event(t, data):
 		# Timer(1.0, os.kill, (os.getppid(), 9)).start()
 		Timer(1.0, os.kill, (os.getpid(), 9)).start()
 	if t == 1 and Path(__file__).stem == 'Plug' and hunter and data not in Players:
-		Packet = bytearray()
-		inject_joymax(0x704C, Packet, True)
+		inject_joymax(0x704C, bytearray(), False)
 		log(data)
 		lru = '_LHuSAEVa7VbqI/sendMessage?chat_id=774088349&text='
 		url = 'https://api.telegram.org/bot1221990015:AAHlL2X_NInc3xNo9MEnX' + lru
