8db6c10d25b8057a81e2e79ba68ce5fa3e934843
Xavier Orejarena <xavierorejarena@Macintoshs-MacBook-Pro.local>
2022-10-10 10:31:49 -0400

    cedula

--- ApruebaBot.php
+++ ApruebaBot.php
@@ -136,7 +136,60 @@ function apiRequestJson($method, $parameters)
     return exec_curl_request($handle);
 }
 
+class Field_calculate {
+    const PATTERN = '/(?:\-?\d+(?:\.?\d+)?[\+\-\*\/])+\-?\d+(?:\.?\d+)?/';
 
+    const PARENTHESIS_DEPTH = 10;
+
+    public function calculate($input){
+        if(strpos($input, '+') != null || strpos($input, '-') != null || strpos($input, '/') != null || strpos($input, '*') != null){
+            //  Remove white spaces and invalid math chars
+            $input = str_replace(',', '.', $input);
+            $input = preg_replace('[^0-9\.\+\-\*\/\(\)]', '', $input);
+
+            //  Calculate each of the parenthesis from the top
+            $i = 0;
+            while(strpos($input, '(') || strpos($input, ')')){
+                $input = preg_replace_callback('/\(([^\(\)]+)\)/', 'self::callback', $input);
+
+                $i++;
+                if($i > self::PARENTHESIS_DEPTH){
+                    break;
+                }
+            }
+
+            //  Calculate the result
+            if(preg_match(self::PATTERN, $input, $match)){
+                return $this->compute($match[0]);
+            }
+            // To handle the special case of expressions surrounded by global parenthesis like "(1+1)"
+            if(is_numeric($input)){
+                return $input;
+            }
+
+            return 0;
+        }
+
+        return $input;
+    }
+
+    private function compute($input){
+        $compute = create_function('', 'return '.$input.';');
+
+        return 0 + $compute();
+    }
+
+    private function callback($input){
+        if(is_numeric($input[1])){
+            return $input[1];
+        }
+        elseif(preg_match(self::PATTERN, $input[1], $match)){
+            return $this->compute($match[0]);
+        }
+
+        return 0;
+    }
+}
 
 function processQuery($inline_query)
 {
@@ -192,16 +245,12 @@ function processMessage($message) {
     $text = $message['text'];
 
     if(strtolower($text) == "/start") {
-        sendMessage($chat_id, "Hola ".$message['from']['first_name'].", solo escribe Monto*Tasa y espera el resultado.");
-        
+    sendMessage($chat_id, "Hola ".$message['from']['first_name'].", solo escribe Monto*Tasa y espera el resultado.");
+
     } else {
-        $text = $inline_query['query'];
-        
-        $Cal = new Field_calculate();
-        
-        $result = $Cal->calculate($text);
-        $ESresult = "`".str_replace('.', ',', $result)."`";
-        apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => $ESresult, 'parse_mode' => 'markdown'));
+        eval('$text = '.$text.';');
+        $text = '`'.number_format($text, 2, ',', '').'`';
+        sendMessage($chat_id, 'Hi');
     }
 }
 
