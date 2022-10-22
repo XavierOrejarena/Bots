10b948a2f72614fb359bd6236f74a74808136470
Xavier Orejarena <xavierorejarena@Macintoshs-MacBook-Pro.local>
2022-10-22 04:58:29 -0400

    cedula

--- ApruebaBot.php
+++ ApruebaBot.php
@@ -223,7 +223,7 @@ function processQuery($inline_query)
             'type'         => 'article',
             'id'           => gen_uuid(),
             'title'        => $ESresult,
-            'message_text' => "`$ESresult`",
+            'message_text' => "`$RES`",
             // 'title'        => "$text = $ESresult",
             // 'message_text' => "$RES \= `$ESresult`",
             'parse_mode'   => 'MarkdownV2',
