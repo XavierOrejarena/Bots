aae094ea1fd7180609fc93d79075721be3dd845f
Xavier Orejarena <xavierorejarena@Macintoshs-MacBook-Pro.local>
2022-10-22 21:09:21 -0400

    cedula

--- ApruebaBot.php
+++ ApruebaBot.php
@@ -220,6 +220,7 @@ function processQuery($inline_query)
         ];
         $text2 = str_replace("*","\*", $text);
         $text2 = str_replace("+","\+", $text);
+        $text2 = str_replace("-","\-", $text);
         $results[] = [
             'type'         => 'article',
             'id'           => gen_uuid(),
