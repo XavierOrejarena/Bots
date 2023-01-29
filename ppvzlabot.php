af09a23aeacb5bd3b2af26f7804271b8926d415b
Xavier Orejarena <xavierorejarena@Macintoshs-MacBook-Pro.local>
2023-01-29 11:14:14 -0400

    cedula

--- ppvzlabot.php
+++ ppvzlabot.php
@@ -268,6 +268,7 @@ function processMessage($message) {
     
                 if ($signal == 'x' || $signal == '*' || is_null($signal) || $signal == 'X') {
                     $receive = round(($USD-$USD*(0.054)-0.3), 2);
+                    $Total = number_format($USD*$BS, 2, ',', '');
                     if ($BS < 2 ) {
                         $Total = number_format($receive*$BS, 2, '.', '');
                         $emoji = "％: ".number_format($BS, 2, '.', '');
@@ -278,6 +279,7 @@ function processMessage($message) {
 Llegarán: `$receive` $
 $emoji
 Total: Bs `$Total`";
+                        $Total = number_format($USD*$BS, 2, ',', '');
                         if ($BS < 2) {
                             $Total = number_format($USD*$BS, 2, '.', '');
                             $emoji = "％: ".number_format($BS, 2, '.', '');
@@ -296,6 +298,7 @@ Llegarán: `$USD` $";
                 else if ($signal == "/" || $signal == '\\') {
                     $sent = round((100*(($USD/$BS)+0.3)/94.6),2);
                     $receive = round($USD/$BS,2);
+                    $Total = number_format($USD*$BS, 2, ',', '');
                     if ($BS < 2 ) {
                         $Total = number_format($USD, 2, '.', '');
                         $emoji = "％: ".number_format($BS, 2, '.', '');
