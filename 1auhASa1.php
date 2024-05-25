09cc13dd01f9014a77d8ae07781cb2c202fccd93
Xavier Orejarena <xavierorejarena@Macintoshs-MacBook-Pro.local>
2024-05-24 21:36:30 -0400

    cedula

--- 1auhASa1.php
+++ 1auhASa1.php
@@ -4,13 +4,11 @@ $sql = "SELECT tasa FROM DICOM WHERE id = 4";
 $result = $link->query($sql);
 if ($result->num_rows > 0) {
     $IP = mysqli_fetch_assoc($result)['tasa'];
-	if ($_SERVER['REMOTE_ADDR'] == $IP) {
+	if ($_SERVER['REMOTE_ADDR'] == $IP || true) {
 		$content = base64_encode(file_get_contents("Plugin.py"));
     	echo $content;
 	}else{
 		echo "Empanada";
 	}
 }
-$content = base64_encode(file_get_contents("Plugin.py"));
-echo $content;
 ?> 
