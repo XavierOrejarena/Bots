524755aba825f49b7aed7d2b5572c9a0503ac404
XavierOrejarena <7832637+XavierOrejarena@users.noreply.github.com>
2024-11-19 11:10:41 -0400

    Update ApruebaBot.php
    despues del hack xd

--- ApruebaBot.php
+++ ApruebaBot.php
@@ -1,9 +1,11 @@
 #!/usr/bin/env php
 <?php
 //@apruebabot
+
 define('BOT_TOKEN', '695950939:');
-define('TOKEN_BOT', 'AAHfKc9Lv1yceBT9yPkpcxNlAeRsLPuFGHw');
-define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.TOKEN_BOT.'/');
+define('TOKEN_BOT', 'AAEkg4Aou');
+define('API_BOT', '-eBPzOE6Ux5vTpdr6pPru6sWJc');
+define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.TOKEN_BOT.API_BOT.'/');
 define('WEBHOOK_URL', 'https://vps239318.vps.ovh.ca/xavier/ApruebaBot.php');
 
 
