64707a82ef8a6587946e5e7d80b4d78118ad4ca3
XavierOrejarena <7832637+XavierOrejarena@users.noreply.github.com>
2025-01-01 13:45:47 -0400

    Update alarm.php

--- alarm.php
+++ alarm.php
@@ -3,13 +3,10 @@
 	$tkn = "411509742:";
 	$token = 'AAFKzBXmOO0fx8e3bXDHBo4tj-QMazfre2E';
 	$api = $tkn.$token;
-
-	$arrContextOptions=array("ssl"=>array("verify_peer"=>false,"verify_peer_name"=>false,),);
-
 	function sendMessage($chat_id, $text)
 	{
 	    global $api;
-	    // $get = file_get_contents();
+	    $arrContextOptions=array("ssl"=>array("verify_peer"=>false,"verify_peer_name"=>false,),);
 	    $get = file_get_contents("https://api.telegram.org/bot$api/sendMessage?chat_id=$chat_id&text=".urlencode($text), false, stream_context_create($arrContextOptions));
 	}
 
@@ -22,6 +19,7 @@
 		$seted_price = $row['seted_price'];
 		$type = $row['type'];
 		$row_num = $row['row_num'];
+		$arrContextOptions=array("ssl"=>array("verify_peer"=>false,"verify_peer_name"=>false,),);
 		$price = json_decode(file_get_contents("https://api.binance.com/api/v1/ticker/price?symbol=$coin", false, stream_context_create($arrContextOptions)), true)['price'];
 		if (is_numeric($price)) {
 			$seted_price = floatval($seted_price);
