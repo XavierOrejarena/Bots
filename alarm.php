30c988374fed08bbfdf29bac4a0699a477f33744
XavierOrejarena <7832637+XavierOrejarena@users.noreply.github.com>
2025-01-01 13:34:27 -0400

    Update alarm.php

--- alarm.php
+++ alarm.php
@@ -18,10 +18,8 @@
 		$seted_price = $row['seted_price'];
 		$type = $row['type'];
 		$row_num = $row['row_num'];
-		$coin = 'BTCUSDT';
 		$arrContextOptions=array("ssl"=>array("verify_peer"=>false,"verify_peer_name"=>false,),);
 		$price = json_decode(file_get_contents("https://api.binance.com/api/v1/ticker/price?symbol=$coin", false, stream_context_create($arrContextOptions)), true)['price'];
-		echo $price;
 		if (is_numeric($price)) {
 			$seted_price = floatval($seted_price);
 			$price = floatval($price);
