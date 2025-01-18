3633aadff03f895ac00a8c3d42238fa74c91d1db
XavierOrejarena <7832637+XavierOrejarena@users.noreply.github.com>
2025-01-18 15:06:08 -0400

    Update DicomAlert.php

--- DicomAlert.php
+++ DicomAlert.php
@@ -1,14 +1,15 @@
 #!/usr/bin/env php
 <?php
 
-$opts = array('http'=>array('header' => "User-Agent:MyAgent/1.0\r\n")); 
-//Basically adding headers to the request
-$context = stream_context_create($opts);
-$data = file_get_contents("https://static.blockshift.co/ve_rates.json",false,$context);
+$context = stream_context_create(
+    array(
+        "http" => array(
+            "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+        )
+    )
+);
 
+echo file_get_contents("https://static.blockshift.co/ve_rates.json", false, $context);
 
-
-
-echo "$data";
 
 ?>
