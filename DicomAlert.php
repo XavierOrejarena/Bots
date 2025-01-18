7476b8c91bfb2d248d921fd00c28876b7e6c9072
XavierOrejarena <7832637+XavierOrejarena@users.noreply.github.com>
2025-01-18 10:46:38 -0400

    Update DicomAlert.php

--- DicomAlert.php
+++ DicomAlert.php
@@ -1,15 +1,16 @@
 #!/usr/bin/env php
 <?php
 
-$arrContextOptions = array(
-      "ssl" => array(
-        "verify_peer" => false,
-        "verify_peer_name" => false,
-      )
+$arrContextOptions=array(
+    "ssl"=>array(
+        "verify_peer"=>false,
+        "verify_peer_name"=>false,
+    ),
 );  
-  
-$context = stream_context_create($arrContextOptions);
-$contents = file_get_contents("https://static.blockshift.co/ve_rates.json",false, $context);
-$dolar = json_decode($contents,true)['prom_epv'];
-echo "$dolar";
+
+$data = file_get_contents("http://bcv.org.ve", false, stream_context_create($arrContextOptions));
+preg_match_all('/> USD</', $data, $matches, PREG_OFFSET_CAPTURE);
+$text = substr($data, $matches[0][0][1]+122, 11);
+echo "$text";
+
 ?>
