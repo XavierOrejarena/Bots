bd893753d043442acd0eda1404cea1982aff58e1
XavierOrejarena <7832637+XavierOrejarena@users.noreply.github.com>
2025-01-18 15:16:03 -0400

    Update DicomAlert.php

--- DicomAlert.php
+++ DicomAlert.php
@@ -1,13 +1,6 @@
 #!/usr/bin/env php
 <?php
 
-$url="https://static.blockshift.co/ve_rates.json";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
-$html = curl_exec($ch);
-curl_close($ch);
-echo json_decode($html, true)['prom_epv'];;
+print_r(json_decode(file_get_contents("https://exchange.vcoud.com/coins/latest?type=bolivar&base=usd"), true)[0]['price']);
 
 ?>
