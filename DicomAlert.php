0cd175b838722b82beba9cc53380855e25378118
XavierOrejarena <7832637+XavierOrejarena@users.noreply.github.com>
2025-01-18 15:10:28 -0400

    Update DicomAlert.php

--- DicomAlert.php
+++ DicomAlert.php
@@ -1,15 +1,13 @@
 #!/usr/bin/env php
 <?php
 
-$context = stream_context_create(
-    array(
-        "http" => array(
-            "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
-        )
-    )
-);
-ini_set('user_agent', 'My-Application/2.5');
-echo file_get_contents("https://static.blockshift.co/ve_rates.json", false, $context);
-
+$url="https://static.blockshift.co/ve_rates.json";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+$html = curl_exec($ch);
+curl_close($ch);
+echo $html;
 
 ?>
