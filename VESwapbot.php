86fe7be3e2af9c261d5252acb933abfc9add2e13
XavierOrejarena <7832637+XavierOrejarena@users.noreply.github.com>
2025-02-07 16:58:55 -0400

    Update VESwapbot.php

--- VESwapbot.php
+++ VESwapbot.php
@@ -269,7 +269,7 @@ function processMessage($message) {
                 ['text' => $result2, 'callback_data' => "2"],
                 ['text' => $porcentaje2, 'callback_data' => "3"]];
 
-    apiRequestJson('sendMessage', ['chat_id' => $chat_id, 'reply_markup' => ['inline_keyboard' => $array]]);
+    apiRequestJson('sendMessage', ['chat_id' => $chat_id, 'text' => 'Resultados:', 'reply_markup' => ['inline_keyboard' => $array]]);
     apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => "*BCV:*            `".$result1."`\n\n*Promedio:*   `$result3`\n\n*Paralelo:*   `".$result2."`", "parse_mode" => "markdown"));
 }
 
