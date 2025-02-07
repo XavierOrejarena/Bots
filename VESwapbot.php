78035a26e12b8b1dc46710d89aa6b4a0b4aea8a7
XavierOrejarena <7832637+XavierOrejarena@users.noreply.github.com>
2025-02-07 15:40:42 -0400

    Update VESwapbot.php

--- VESwapbot.php
+++ VESwapbot.php
@@ -258,7 +258,7 @@ function processMessage($message) {
     $porcentaje2 = number_format((1-$tasaBCV/$tasaParallel)*100,2,",","");
     $array = [];
     $array[] =  [['text' => "BCV", 'callback_data' => "xd1"],
-                ['text' => $results1, 'callback_data' => "xd2"],
+                ['text' => "$results1", 'callback_data' => "xd2"],
                 ['text' => "0%", 'callback_data' => "xd3"]];
 
     apiRequestJson('sendMessage', ['chat_id' => $chat_id, 'text' => 'Resultados:', 'reply_markup' => ['inline_keyboard' => $array]]);
