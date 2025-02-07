15284f1de58d01cf190aaa4dd6ab4c52e0ebc459
XavierOrejarena <7832637+XavierOrejarena@users.noreply.github.com>
2025-02-07 16:49:55 -0400

    Update VESwapbot.php

--- VESwapbot.php
+++ VESwapbot.php
@@ -258,14 +258,14 @@ function processMessage($message) {
     $porcentaje2 = number_format((1-$tasaBCV/$tasaParallel)*100,2,",","");
     $array = []
     $array[] =  [[['text' => "BCV", 'callback_data' => "1"],
-                ['text' => $result1, 'callback_data' => "2"],
+                ['text' => '$result1', 'callback_data' => "2"],
                 ['text' => "0%", 'callback_data' => "3"]],
                 [['text' => "Promedio", 'callback_data' => "4"],
-                ['text' => $result3, 'callback_data' => "5"],
-                ['text' => $porcentaje3, 'callback_data' => "6"]],
+                ['text' => '$result3', 'callback_data' => "5"],
+                ['text' => '$porcentaje3', 'callback_data' => "6"]],
                 [['text' => "Paralelo", 'callback_data' => "7"],
-                ['text' => $result2, 'callback_data' => "8"],
-                ['text' => $porcentaje2, 'callback_data' => "9"]]];
+                ['text' => '$result2', 'callback_data' => "8"],
+                ['text' => '$porcentaje2', 'callback_data' => "9"]]];
 
     apiRequestJson('sendMessage', ['chat_id' => $chat_id, 'text' => '       Resultados:', 'reply_markup' => ['inline_keyboard' => $array]]);
     apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => "*BCV:*            `".$result1."`\n\n*Promedio:*   `$result3`\n\n*Paralelo:*   `".$result2."`", "parse_mode" => "markdown"));
