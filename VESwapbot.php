8dde84d502fc7dccc06e2a628344f433a410471c
XavierOrejarena <7832637+XavierOrejarena@users.noreply.github.com>
2025-02-07 15:57:55 -0400

    Update VESwapbot.php

--- VESwapbot.php
+++ VESwapbot.php
@@ -258,8 +258,8 @@ function processMessage($message) {
     $porcentaje2 = number_format((1-$tasaBCV/$tasaParallel)*100,2,",","");
     $array = [];
     $array[] =  [['text' => "BCV", 'callback_data' => "1"],
-                ['text' => "`$result1`", 'callback_data' => "2"],
-                ['text' => "0%", 'callback_data' => "3"], "parse_mode" => "markdown"];
+                ['text' => "`$result1`", 'callback_data' => "2", "parse_mode" => "markdown"],
+                ['text' => "0%", 'callback_data' => "3"]];
 
     apiRequestJson('sendMessage', ['chat_id' => $chat_id, 'text' => 'Resultados:', 'reply_markup' => ['inline_keyboard' => $array]]);
     apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => "*BCV:*            `".$result1."`\n\n*Promedio:*   `$result3`\n\n*Paralelo:*   `".$result2."`", "parse_mode" => "markdown"));
