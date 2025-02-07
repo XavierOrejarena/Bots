759cbc7142a72bc43867627ea41a9e265230a7a1
XavierOrejarena <7832637+XavierOrejarena@users.noreply.github.com>
2025-02-07 15:57:22 -0400

    Update VESwapbot.php

--- VESwapbot.php
+++ VESwapbot.php
@@ -259,9 +259,9 @@ function processMessage($message) {
     $array = [];
     $array[] =  [['text' => "BCV", 'callback_data' => "1"],
                 ['text' => "`$result1`", 'callback_data' => "2"],
-                ['text' => "0%", 'callback_data' => "3"]];
+                ['text' => "0%", 'callback_data' => "3"], "parse_mode" => "markdown"];
 
-    apiRequestJson('sendMessage', ['chat_id' => $chat_id, 'text' => 'Resultados:', 'reply_markup' => ['inline_keyboard' => $array,"parse_mode" => "markdown"]]);
+    apiRequestJson('sendMessage', ['chat_id' => $chat_id, 'text' => 'Resultados:', 'reply_markup' => ['inline_keyboard' => $array]]);
     apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => "*BCV:*            `".$result1."`\n\n*Promedio:*   `$result3`\n\n*Paralelo:*   `".$result2."`", "parse_mode" => "markdown"));
 }
 
