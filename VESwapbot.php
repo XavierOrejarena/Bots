ea12117cb187a3cdd993f0e25609b78ce159caf4
XavierOrejarena <7832637+XavierOrejarena@users.noreply.github.com>
2025-02-07 12:28:13 -0400

    Update VESwapbot.php

--- VESwapbot.php
+++ VESwapbot.php
@@ -254,13 +254,13 @@ function processMessage($message) {
 
     $result3 = $text*($tasaBCV+$tasaParallel)/2;
     $result3 = number_format($result3, 2, ',', '');
-    apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => "<pre>
+    apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => "
 | Tasa   |      Monto      |
 |----------|:---------------:|
 | BCV      |    ```$result1```     |
 | Promedio |    ```$result3```     |
 | Paralelo |    ```$result2```     |
-</pre>", "parse_mode" => "MarkdownV2"));
+", "parse_mode" => "MarkdownV2"));
 }
 
 $content = file_get_contents('php://input');
