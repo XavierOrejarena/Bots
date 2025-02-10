0064180feac849447fa0ac1f18cdcad29ecac62c
XavierOrejarena <someone@someplace.com>
2025-02-09 22:05:20 -0400

    xd

--- Silkroad.php
+++ Silkroad.php
@@ -1,4 +1,4 @@
 <?php
-$data = file_get_contents("php://input");
-file_put_contents("test.json", $data)
+$data = json_encode(file_get_contents("php://input"));
+file_put_contents("test.json", json_decode($data))
 ?>
