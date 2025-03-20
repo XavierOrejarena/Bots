fa65fee17d174832a58bcbe94db34b11dc324a3f
name <myemail@domain.com>
2025-03-20 17:25:11 -0400

    xd

--- VESwapbot.php
+++ VESwapbot.php
@@ -270,7 +270,7 @@ function processMessage($message) {
                 ['text' => $porcentaje2, 'callback_data' => $result2]];
 
     // apiRequestJson('sendMessage', ['chat_id' => $chat_id, 'text' => 'Resultados:', 'reply_markup' => ['inline_keyboard' => $array]]);
-    apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => "$text USD
+    apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => "      $text USD
 
 *BCV:*            `".$result1."`\n\n*Promedio:*   `$result3`\n\n*Paralelo:*   `".$result2."`", "parse_mode" => "markdown"));
 }
