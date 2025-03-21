5b26b0543aa9e63722c849971edfbee4e0527683
name <myemail@domain.com>
2025-03-21 00:28:28 -0400

    xd

--- the_binance_bot.php
+++ the_binance_bot.php
@@ -247,6 +247,7 @@ If you want to see all your alarms /MYALARMS");
 
     }
     elseif (strtolower($text) == "/myalarms") {
+        sendMessage($chat_id,$chat_id);
         include "connect.php";
         $result = mysqli_query($link, "SELECT * FROM alarms_binance WHERE chat_id = $chat_id ORDER BY seted_price ASC");
         if (mysqli_fetch_array($result)) {
