a8c8ae1bf7cf1d69c300bcc3de291a9e333e6184
name <myemail@domain.com>
2025-03-20 23:47:41 -0400

    xd

--- the_binance_bot.php
+++ the_binance_bot.php
@@ -220,7 +220,7 @@ Upper case and lower case does not matter.
 Any bug or suggestion @XavierOrejarena');
     }
     elseif (strtolower($text) == "/coins") {
-        sendMessage($chat_id, "1565852343134184106")
+        sendMessage($chat_id, "1565852343134184106");
         // include "connect.php";
         // mysqli_query($link, "UPDATE alarms_binance SET type=type+1 WHERE row_num=2028");
     }
