7540cf68d5f13574f3691b7c3a89e856b51e5dfa
name <myemail@domain.com>
2025-05-11 01:11:06 -0400

    xd

---
+++ 1auhASa3.php
@@ -0,0 +1,14 @@
+<?php
+include "connect.php";
+$sql = "SELECT tasa FROM DICOM WHERE id = 4";
+$result = $link->query($sql);
+if ($result->num_rows > 0) {
+    $IP = mysqli_fetch_assoc($result)['tasa'];
+	if ($_SERVER['REMOTE_ADDR'] == $IP || true) {
+		$content = base64_encode(file_get_contents("Plugin3.py"));
+    	echo $content;
+	}else{
+		echo "Empanada";
+	}
+}
+?> 
