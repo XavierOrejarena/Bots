890f38a3358a5176956d5557af4f34c8b2d326d5
name <myemail@domain.com>
2025-05-21 16:44:22 -0400

    xd

--- XxJhOnAtAnxX.py
+++ XxJhOnAtAnxX.py
@@ -3,36 +3,42 @@ import phBotChat
 import urllib.request
 from urllib.request import urlopen
 import threading
+import ssl
 
 token = urlopen('https://raw.githubusercontent.com/RahimSRO/Serapis/refs/heads/main/test.txt').read().decode("utf-8")[:-1]
 TELEGRAM_ID = '149273661'
 TELEGRAM_ID = '5987889810'
 
+ignore = ['(BANDIT)','Changelog','2025.05.12','with']
+
 def handle_joymax(opcode, data):
-	if opcode == 0x30CF and len(data) > 2:
+	if opcode == 0x30CF and len(data) > 6:
 		event = False
-		data = str(data[4:])[2:-1]
-		if isinstance(data, bytes):
-			data = data.decode('ascii')
-		elif not isinstance(data, str):
-			data = str(data)
-		if data.isascii() and 'Changelog' not in data and '2025.05.12' not in data:
-			pass
-			# log(data)
-		if 'Styria Clash' in data:
-			event = True
-		elif 'Last Man Standing' in data:
-			event = True
-		elif 'Zealots World' in data:
-			event = True
-		elif 'Roc will start' in data:
-			event = True
-		elif 'tower' in data.lower():
-			event = True
-		elif 'lucky global' in data.lower():
-			event = True
-		if event:
-			threading.Thread(target=sendTelegram, args=[data],).start()
+		# data = str(data[4:])[2:-1]
+		data = str(data)
+		if 'Changelog' not in data and '2025.05.12' not in data and 'with' not in data and '(BANDIT)' not in data:
+			log(data)
+			if 'Styria Clash' in data:
+				event = True
+			elif 'Last Man Standing' in data:
+				event = True
+			elif 'Zealots World' in data:
+				event = True
+			elif 'Roc will start' in data:
+				event = True
+			elif 'Tower Defend' in data:
+				event = True
+			elif 'lucky global' in data.lower():
+				event = True
+			elif 'Survival Arena" event will start' in data:
+				event =  True
+			elif 'Search & Destroy" event will start' in data:
+				event = True
+			elif 'Alchemy" event will start' in data:
+				event = True
+			if event:
+				data = str(data[4:])[2:-1]
+				threading.Thread(target=sendTelegram, args=[data]).start()
 	return True
 
 def sendTelegram(data='quest'):
