87163014aeb37f6fbbdb68f33fe45bc02dfddb6d
name <myemail@domain.com>
2025-05-22 13:59:51 -0400

    xd

--- XxJhOnAtAnxX.py
+++ XxJhOnAtAnxX.py
@@ -30,7 +30,7 @@ def handle_joymax(opcode, data):
 				event = True
 			elif 'Tower Defend' in msg:
 				event = True
-			elif 'lucky global' in msg.lower():
+			elif 'Lucky Global' in msg:
 				event = True
 			elif 'Survival Arena" event will start' in msg:
 				event =  True
@@ -52,6 +52,29 @@ def handle_joymax(opcode, data):
 		if uniqueName in UNIQUES:
 			threading.Thread(target=sendTelegram, args=[uniqueName]).start()
 		log(uniqueName)
+	# elif opcode == 0x3068 and get_character_data()['name'] in lideres: #party item droped distributed
+	# 	itemName = get_item(struct.unpack_from('<I', data, 4)[0])['name']
+	# 	playerName = get_party()[struct.unpack_from('<I', data, 0)[0]]['name']
+	# 	if struct.unpack_from('<I', data, 4)[0] > 33892 and struct.unpack_from('<I', data, 4)[0] < 33901:
+	# 		msg = f'item [{itemName}] is distributed to [{playerName}]'
+	# 		azulPerma(msg)
+	# 		sendTelegram(f'item `{itemName}` is distributed to `{playerName}`')
+	elif opcode == 0xB034 and len(data) > 11 and get_character_data()['name'] in lideres: #item gained
+		dropType = struct.unpack_from('h', data, 0)[0]
+		if dropType == 7169 or dropType == 4353:
+			itemID = get_item(struct.unpack_from('I', data, 11)[0])
+			itemName = itemID['name']
+			if struct.unpack_from('I', data, 11)[0] > 33892 and struct.unpack_from('I', data, 11)[0] < 33901:
+				azulPerma(f'item [{itemName}] gained.')
+				playerName = get_character_data()['name']
+				sendTelegram(f'`{itemName}` Gained. ---> `{playerName}`')
+		elif dropType == 1537:
+			itemID = get_item(struct.unpack_from('I', data, 7)[0])
+			itemName = itemID['name']
+			if struct.unpack_from('I', data, 7)[0] > 33892 and struct.unpack_from('I', data, 7)[0] < 33901:
+				azulPerma(f'item [{itemName}] gained.')
+				playerName = get_character_data()['name']
+				sendTelegram(f'`{itemName}` Gained. ---> `{playerName}`')
 	return True
 
 def sendTelegram(data='quest'):
@@ -68,7 +91,11 @@ def handle_chat(t,player,msg):
 	if t == 2 and get_character_data()['name'] == name:
 		threading.Thread(target=sendTelegram, args=[player + " -> " + get_character_data()['name'] + ' -> ' + msg]).start()
 
-log('Event plugin v0.2.0 loeaded...')
+def handle_event(t, data):
+	if t == 5:
+		threading.Thread(target=sendTelegram, args=['*'+get_character_data()['name'] + '* -> `'+get_item(int(data))['name']+'`'],).start()
+
+log('Event plugin v3.0 loeaded...')
 
 # data = bytes.fromhex('15 01 1F 00 22 54 6F 77 65 72 20 44 65 66 65 6E 64 22 20 65 76 65 6E 74 20 68 61 73 20 65 6E 64 65 64 2E')
 # data = bytes.fromhex('15 01 2E 00 22 50 76 50 20 4D 61 74 63 68 69 6E 67 22 20 65 76 65 6E 74 20 77 69 6C 6C 20 73 74 61 72 74 20 69 6E 20 31 30 20 6D 69 6E 75 74 65 73 2E')
