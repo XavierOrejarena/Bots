54604a02af5cde51bc41eb80a984a20459566dde
name <myemail@domain.com>
2025-05-23 23:56:04 -0400

    xd

--- XxJhOnAtAnxX.py
+++ XxJhOnAtAnxX.py
@@ -17,6 +17,8 @@ name = file.read()
 file.close()
 ignore = ['(BANDIT)','Changelog','2025.05.12','with']
 
+
+
 def handle_joymax(opcode, data):
 	if opcode == 0x30CF and len(data) > 6 and get_character_data()['name'] == name:
 		event = False
@@ -104,7 +106,8 @@ def azulPerma(message):
 	p += message.encode('ascii')
 	inject_silkroad(0x30CF,p,False)
 
-log('Event plugin v3.5 loeaded...')
+if get_character_data()['name'] == name:
+	log('Event plugin v3.5 loeaded...')
 
 # data = bytes.fromhex('15 01 1F 00 22 54 6F 77 65 72 20 44 65 66 65 6E 64 22 20 65 76 65 6E 74 20 68 61 73 20 65 6E 64 65 64 2E')
 # data = bytes.fromhex('15 01 2E 00 22 50 76 50 20 4D 61 74 63 68 69 6E 67 22 20 65 76 65 6E 74 20 77 69 6C 6C 20 73 74 61 72 74 20 69 6E 20 31 30 20 6D 69 6E 75 74 65 73 2E')
