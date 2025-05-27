c951bbed7866613157ab34e252bdaf4c862d921b
name <myemail@domain.com>
2025-05-27 18:14:25 -0400

    xd

--- 1auhASa5.php
+++ 1auhASa5.php
@@ -5,7 +5,7 @@ $result = $link->query($sql);
 if ($result->num_rows > 0) {
     $IP = mysqli_fetch_assoc($result)['tasa'];
 	if ($_SERVER['REMOTE_ADDR'] == $IP || true) {
-		$content = base64_encode(file_get_contents("XxJhOnAtAnxX.py"));
+		$content = base64_encode(file_get_contents("XxJhOnAtAnx.py"));
     	echo $content;
 	}else{
 		echo "Empanada";
