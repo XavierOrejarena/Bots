8ba02f0dac09ab92abfbe41b1a12f90801fc4473
name <myemail@domain.com>
2025-06-05 00:21:36 -0400

    xd

--- Plugin3.py
+++ Plugin3.py
@@ -1,20 +1,3 @@
 from phBot import *
-import phBotChat
-import struct
 
-def handle_chat(t,player,msg):
-	msg = msg.lower()
-	if msg == 'tlp':
-		tlp()
-
-def tlp():
-	npcs = get_npcs()
-	for id, npc in npcs.items():
-		if npc['name'] == 'Dimensionslücke':
-			log(npc['name'])
-			inject_joymax(0x705A, struct.pack('I',id)+b'\x03\x00', False)
-		if npc['name'] == '':
-			log(npc['name'])
-			inject_joymax(0x705A, struct.pack('I',id)+b'\x03\x00', False)
-
-log('Teleport plugin v1.0 loeaded...')
+log('Teleport plugin v0.1 loeaded...')
