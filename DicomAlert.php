b64fad611a55e090be80c8ba9dbe48e45584869d
name <p@mail.com>
2025-09-29 11:43:25 -0400

    xd ultimo final final ahora si

--- DicomAlert.php
+++ DicomAlert.php
@@ -76,7 +76,7 @@ function USD(){
 	$text = (string)$text;
 	print_r("USD: ".$text."\n");
 	if ($text =! ""){
-		echo "UESEDE: ".$text."\n";
+		print_r("UESEDE: ".$text."\n");
 		return $text;
 	} else{
 		return USD();
@@ -102,7 +102,7 @@ function EUR(){
 	$text = (string)$text;
 	print_r("EUR: ".$text."\n");
 	if ($text =! ""){
-		echo "EURILLO: ".$text."\n";
+		print_r("EURILLO: ".$text."\n");
 		return $text;
 	} else{
 		return EUR();
